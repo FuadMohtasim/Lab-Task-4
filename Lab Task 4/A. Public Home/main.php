4f81070d6bd6a0a8051595c0ad0bc62f81a95464
Md. Mohtasim Fuad <91050280+FuadMohtasim@users.noreply.github.com>
2022-10-29 21:00:58 +0600

    Add files via upload

---
+++ Lab Task 4/A. Public Home/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+		include 'navbar.php';
+	 ?>
+	<fieldset>
+	<p>Welcome to Cineplex BD</p>
+	</fieldset>
+	<?php 
+		include 'footer.php';
+	 ?>
+</body>
+</html>
---
+++ Lab Task 4/D. Forgot Password/navbar.php
@@ -0,0 +1,10 @@
+<fieldset>
+<?php 
+	echo "<h1>Cineplex BD</h1>";
+ ?>
+ <center>
+ <a href="http://localhost:8080/Web%20Technologies/Lab%20Task%204/A.%20Public%20Home/main.php">Home</a> |
+ <a href="http://localhost:8080/Web%20Technologies/Lab%20Task%204/B.%20Registration/store.php">Registration</a> |
+ <a href="http://localhost:8080/Web%20Technologies/Lab%20Task%204/C.%20Login/login.php">Login</a>
+</center>
+</fieldset>
